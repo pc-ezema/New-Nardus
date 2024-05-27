fe012282b03c7ec15ea26d9ee38bf09e782c07a5
Promise <promiseezema11@gmail.com>
2024-05-27 16:32:45 +0100

    updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -25,22 +25,22 @@
                     Keep up to date on social media
                 </p>
                 <div id="footer__some-container">
-                    <a href="https://www.linkedin.com/showcase/iff-bioscience/" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-9691" role="img">
+                    <a href="3" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-9691" role="img">
                             <desc id="desc-9691"></desc>
                             <use xlink:href="typo3temp/assets/svg/7c37810a546deeab88bf7901f679e1c2.svg#icon-linkedin"></use>
-                        </svg></a><a href="https://www.youtube.com/channel/UC_Vyt7tViA5GP7_-okrbatw" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-6632" role="img">
+                        </svg></a><a href="" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-6632" role="img">
                             <desc id="desc-6632"></desc>
                             <use xlink:href="typo3temp/assets/svg/7c37810a546deeab88bf7901f679e1c2.svg#icon-youtube"></use>
-                        </svg></a><a rel="noopener" href="contact/wechat-links.html" target="_blank"><svg id="" class="" aria-labelledby="desc-947" role="img">
+                        </svg></a><a rel="noopener" href="#" target="_blank"><svg id="" class="" aria-labelledby="desc-947" role="img">
                             <desc id="desc-947"></desc>
                             <use xlink:href="typo3temp/assets/svg/7c37810a546deeab88bf7901f679e1c2.svg#icon-wechat"></use>
-                        </svg></a><a href="https://www.facebook.com/InternationalFlavorsandFragrances" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-9461" role="img">
+                        </svg></a><a href="#" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-9461" role="img">
                             <desc id="desc-9461"></desc>
                             <use xlink:href="typo3temp/assets/svg/7c37810a546deeab88bf7901f679e1c2.svg#icon-facebook"></use>
-                        </svg></a><a href="https://twitter.com/iff" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-5752" role="img">
+                        </svg></a><a href="#" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-5752" role="img">
                             <desc id="desc-5752"></desc>
                             <use xlink:href="typo3temp/assets/svg/7c37810a546deeab88bf7901f679e1c2.svg#icon-twitter"></use>
-                        </svg></a><a href="https://www.instagram.com/iffinc/" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-3765" role="img">
+                        </svg></a><a href="#" target="_blank" rel="noopener noreferrer"><svg id="" class="" aria-labelledby="desc-3765" role="img">
                             <desc id="desc-3765"></desc>
                             <use xlink:href="typo3temp/assets/svg/7c37810a546deeab88bf7901f679e1c2.svg#icon-instagram"></use>
                         </svg></a>
